7cd8988505d99e8958c1a5f17615547ade0e59ce
Mateus Lira <mateus.lopes39@etec.sp.gov.br>
2025-04-29 15:16:13 -0300

    aula

--- aulas/validacao_jquery/validacao_jquery/js/validacao.js
+++ aulas/validacao_jquery/validacao_jquery/js/validacao.js
@@ -1,38 +1,66 @@
-//validação simples
-//$("#formExemplo").validate();
-
-$("#formExemplo").validate({
-    debug: true,
+$(document).ready(function () {
+  $("#cadastroFuncionario").validate({
     rules: {
-        txtemail: {
-            required: true,
-            email: true,
-        },
-        txtnome: {
-            required: true,
-            rangelength: [2, 50],
-        },
-        txtidade: {
-            min: 18,
-            max: 100,
-            required: true,
-            //range: [18,100],
-        }
+      nome: {
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      filhos: {
+        required: true,
+        number: true,
+        min: 0,
+        max: 10
+      },
+      salario: {
+        required: true,
+        number: true,
+        min: 2000,
+        max: 10000,
+      },
+      admissao: {
+        required: true,
+        date: true
+      },
+      social: {
+        required: true,
+        url: true
+      }
     },
     messages: {
-        txtemail: {
-            required: "E-mail obrigatório",
-            email: "Formato inválido de e-mail",
-        },
-        txtnome: {
-            required: "Nome obrigatório",
-            minlength: "Exigido 2 caracteres",
-            maxlength: "Até 50 caracteres",
-        },
-        txtidade:{
-            required: "Idade obrigatória",
-            min: "Valor mínimo: 18",
-            max: "Valor máximo: 100",
-        }
-    }
+      nome: {
+        required: "O campo Nome é obrigatório.",
+        minlength: "O nome deve ter pelo menos 5 caracteres.",
+        maxlength: "O nome pode ter no máximo 50 caracteres."
+      },
+      email: {
+        required: "O campo E-mail é obrigatório.",
+        email: "Informe um e-mail válido."
+      },
+      filhos: {
+        required: "Informe o número de filhos.",
+        number: "Informe um número válido.",
+        min: "O número de filhos não pode ser menor que 0.",
+        max: "O número de filhos não pode ser maior que 10."
+      },
+      salario: {
+        required: "Informe o salário.",
+        number: "Informe um valor numérico válido.",
+        min: "O salário mínimo é R$ 2000.",
+        max: "O salário máximo é R$ 10000.",
+      },
+      admissao: {
+        required: "Informe a data de admissão.",
+        date: "Informe uma data válida."
+      },
+      social: {
+        required: "Informe o endereço da rede social.",
+        url: "Informe uma URL válida (ex: https://...)."
+      }
+    },
+  });
 });
